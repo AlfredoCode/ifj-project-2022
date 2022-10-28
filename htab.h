12ed9c5d1ea414b91c12159f501047caac591d09
safar.radim <xsafar27@stud.fit.vutbr.cz>
2022-10-28 17:46:09 +0200

    Edited htab.h to accept stat_t

--- htab.h
+++ htab.h
@@ -1,7 +1,7 @@
 /*
  * ======================================================== *
  * Name:       htab.h                                       *
- * Last modif: 10/17/2022                                   *
+ * Last modif: 10/28/2022                                   *
  * ======================================================== *
  */
 
@@ -15,20 +15,15 @@
 
 #include <string.h>     // size_t
 #include <stdbool.h>    // bool
-#include "scanner.h"
+#include "parser.h"
 
 typedef const char * htab_key_t;        // typ klíče
 typedef int htab_value_t;               // typ hodnoty
 
-// Dvojice dat v tabulce:
-typedef struct htab_pair {
-    htab_key_t    key;          // klíč
-    htab_value_t  value;        // asociovaná hodnota
-} htab_pair_t;                  // typedef podle zadání
 
 // Struktury
 typedef struct htab_item {
-    htab_pair_t pair;
+    stat_t statement;
     struct htab_item *next;
 } htab_item_t;
 
@@ -49,14 +44,14 @@ size_t htab_bucket_count(const htab_t * t);     // velikost pole
 void htab_resize(htab_t *t, size_t newn);       // změna velikosti pole
                                                 // (umožňuje rezervaci místa)
 
-htab_pair_t * htab_find(htab_t * t, htab_key_t key);  // hledání
-htab_pair_t * htab_lookup_add(htab_t * t, htab_key_t key);
+stat_t * htab_find(htab_t * t, htab_key_t key);  // hledání
+stat_t * htab_lookup_add(htab_t * t, htab_key_t key);
 
 bool htab_erase(htab_t * t, htab_key_t key);    // ruší zadaný záznam
 
 // for_each: projde všechny záznamy a zavolá na ně funkci f
 // Pozor: f nesmí měnit klíč .key ani přidávat/rušit položky
-void htab_for_each(const htab_t * t, void (*f)(htab_pair_t *data));
+void htab_for_each(const htab_t * t, void (*f)(stat_t *data));
 
 void htab_clear(htab_t * t);    // ruší všechny záznamy
 void htab_free(htab_t * t);     // destruktor tabulky
