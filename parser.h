06b2d88797cb69347be338a617af5eddb44ca610
safar.radim <xsafar27@stud.fit.vutbr.cz>
2022-10-28 17:31:29 +0200

    Replaced structures with one overall stat_t structure

--- parser.h
+++ parser.h
@@ -2,7 +2,6 @@
  * =================================================== *
  * Name:       parser.h                                *
  * Authors:    xsafar27                                * 
- *             xlogin00
  * Last modif: 10/24/2022                              *
  * =================================================== *
  */
@@ -10,16 +9,20 @@
 #ifndef __PARSER_H__
 #define __PARSER_H__
 
-typedef struct var {
-    int type; //enum
-    char* name;
-    void* val;
-} var_t;
+// Enum for statement types
+enum stat_type {
+    t_fun,
+    t_int,
+    t_float,
+    t_str,
+};
 
-typedef struct function {
+// Internal structure used to store variables and functions inside htab
+typedef struct statement {
+    stat_type type;
     char* name;
-    int ret; //enum
-    var_t *args;
-} function_t;
+    char* value;
+} stat_t;
 
-#endif
+#endif  // __PARSER_H__
+
