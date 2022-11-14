fe2b51588b3ac362c47b1f7111a72a7561b9387b
safar.radim <xsafar27@stud.fit.vutbr.cz>
2022-11-14 04:07:39 +0100

    Added err_msg

---
+++ error.c
@@ -0,0 +1,19 @@
+/*
+ * =================================================== *
+ * Name:       error.c                                 *
+ * Authors:    xsafar27                                * 
+ * Last modif: 11/14/2022                              *
+ * =================================================== *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "error.h"
+
+void err_msg(err_code err, char* message)
+{
+    fprintf(stderr, "Exit code (%i): %s\n", err, message);
+    // exit(err);
+}
+
