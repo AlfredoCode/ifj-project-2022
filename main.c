270e34bf3dde1f21deb852225810106d744a9ddb
AlfredoCode <gormeet@seznam.cz>
2022-11-19 21:27:48 +0100

    plz help

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
-#include "parser.h"
+
 #include "scanner.h"
+#include "parser.h"
 #include "error.h"
 
 
 int main(){
     int err_code = 0;
     err_code = parse();
+
+
     if(err_code == SUCCESS_ERR){
         // SUCCESSFUL PARSE
-        fprintf(stderr,"Well done my man\n");
+        fprintf(stderr,"Compiled successfully\n");
         return err_code;
     }
     fprintf(stderr,"Could not compile the code\n");
     return err_code;
+
 
 
 }
