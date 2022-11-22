a9791d1ba3cfc4619d72ca63bf16ad43de656434
safar.radim <xsafar27@stud.fit.vutbr.cz>
2022-11-22 23:44:18 +0100

    created generator files

---
+++ generator.h
@@ -0,0 +1,28 @@
+/*
+ * =================================================== *
+ * Name:       generator.h                             *
+ * Authors:    xsafar27                                * 
+ * Last modif: 11/22/2022                              *
+ * =================================================== *
+ */
+
+#ifndef __GENERATOR_H__
+#define __GENERATOR_H__
+
+// Generate built-ins
+// Reads
+// Readi
+// Readf
+// Write
+// Strlen
+// Substring
+// Ord
+// Chr
+
+// Functions
+
+// Gen init
+
+// ...
+
+#endif // __GENERATOR_H__
