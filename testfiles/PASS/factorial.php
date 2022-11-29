87063d529d0ef133eeabd1179d2b5ea772a78200
AlfredoCode <gormeet@seznam.cz>
2022-11-30 00:11:34 +0100

    Testfiles added

---
+++ testfiles/PASS/factorial.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+// Program 1: Vypocet faktorialu (iterativne)
+
+// Hlavni telo programu
+write("Zadejte cislo pro vypocet faktorialu");
+// $a = readi();
+
+if ($a === null) {
+	write("Chyba pri nacitani celeho cisla!\n");
+} else {}
+
